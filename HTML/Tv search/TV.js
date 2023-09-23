d2556e0de5687ef4c02f15b69307fc3f980ed57a
Aslan <abullahaslan3010@gmail.com>
2023-09-23 15:16:39 +0300

    DAAAMn

--- HTML/Tv search/TV.js
+++ HTML/Tv search/TV.js
@@ -6,23 +6,35 @@ const form = document.querySelector("form")
 form.addEventListener("submit", async function (e) {
 	e.preventDefault()
 	try {
+		deleter()
 		const seaerch = form.elements.Qol.value
 		const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${seaerch}`)
-		const rd = res.data
-		imagemaker(rd)
-		
+		imagemaker(res.data)
+
 
 	} catch (error) {
 		console.log("not found !!")
 	}
 
 	function imagemaker(shows) {
+		const line = document.createElement('li')
 
+		document.body.append(line)
 		for (let show of shows) {
 			const img = document.createElement('img')
 			img.src = show.show.image.medium;
+
+
 			document.body.append(img)
 		}
+
+	}
+	function deleter() {
+		const images = document.querySelectorAll('img')
+		for (let image of images) {
+			image.remove()
+		}
+
 
 	}
 

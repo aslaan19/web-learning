cdbea7d740c3978f0358b827269c62f810833cf5
Aslan <abullahaslan3010@gmail.com>
2023-09-24 20:54:40 +0300

    leader

--- HTML/OOP practis/OOP.js
+++ HTML/OOP practis/OOP.js
@@ -3,19 +3,33 @@ const Aslan = {
 	name: "Abdullah",
 	age: 15,
 
-	getData : function getData() {
+	/* etData : function getData() {
 		return `he is ${this.name}  and he is ${this.age} years old !`	
 	},
 	Isleader : function Isleader(params) {
 		return` He ${this.leader ? "is " : "is not "} a leader`
-	}
+	} */
 
 }
 
-const helal = Object.create(Aslan)
-const omar = Object.assign({},Aslan)
+//const helal = Object.create(Aslan)
+//const omar = Object.assign({},Aslan)
+let data = ""
+let count = 0
+const H= document.querySelector('H1')
+for(let info in Aslan){
+	data += ` the ${info} is ${Aslan[info]}`
+	count ++
+	if(count ==2){
+		H.innerHTML = `The leader is ${Aslan[info]}`
+	}
+	
+	
+}
 
-omar.leader = false
 
+console.log(data)
+const div = document.querySelector('div')
+div.innerHTML = data
 
 

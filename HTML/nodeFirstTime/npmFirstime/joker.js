0c40d8f1572204b4a2fd68006cff15037ad728f3
Aslan <abullahaslan3010@gmail.com>
2023-09-30 02:10:25 +0300

    Aslan

---
+++ HTML/nodeFirstTime/npmFirstime/joker.js
@@ -0,0 +1,8 @@
+const joks = require('give-me-a-joke')
+//console.log(joks)
+// To get a random dad joke
+joks.getRandomDadJoke (function(joke) {
+   console.log(joke);
+});
+
+// To get a random Chuck Norris joke

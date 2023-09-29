48e789e79f32277317581f47154c895c1dbe565a
Aslan <abullahaslan3010@gmail.com>
2023-09-30 02:47:50 +0300

    asd

--- HTML/nodeFirstTime/npmFirstime/joker.js
+++ HTML/nodeFirstTime/npmFirstime/joker.js
@@ -1,8 +1,14 @@
 const joks = require('give-me-a-joke')
-//console.log(joks)
-// To get a random dad joke
+const colors = require('colors')
+const cow = require('cowsay')
+
 joks.getRandomDadJoke (function(joke) {
-   console.log(joke);
+   console.log(joke.black.bgWhite);
 });
 
+console.log(cow.say({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}).rainbow.bgYellow);
 // To get a random Chuck Norris joke
